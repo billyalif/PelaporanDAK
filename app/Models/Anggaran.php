0eb890b22c8ea0b9f72bd63bff74a520cb5bfe57
faraahsm <85088236+faraahsm@users.noreply.github.com>
2022-07-18 14:14:51 +0700

    anggaran

--- app/Models/Anggaran.php
+++ app/Models/Anggaran.php
@@ -5,7 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Anggaran extends Model
-{
+
+class Anggaran extends Model{
+
     use HasFactory;
+
+    protected  $table = 'anggaran';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'jenis_anggaran',
+        'volume',
+        'satuan',
+        'harga_satuan',
+        'jumlah_penerima_manfaat',
+        'pagu_diterima',
+        'pagu_dibelanjakan'
+    ];
+    
+    //relationship
+    public function Batch(){
+        //just having one batch
+        return $this->belongsTo(Batch::class, 'FK_id_batch', 'id');
+    }
+    
+    public function Dak(){
+        //just having one dak
+        return $this->belongsTo(Dak::class,'FK_id_dak', 'id');
+    }
+
+    public function Kegiatan(){
+        //just having one kegiatan
+        return $this->belongsTo(Kegiatan::class, 'FK_id_kegiatan', 'id');
+    }
+  
 }
+?>
