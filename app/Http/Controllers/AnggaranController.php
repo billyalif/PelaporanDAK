65d9c25299f77fc631921e66987e8eba4efb32de
billyalif <muhammad.nabil.alif-2020@vokasi.unair.ac.id>
2022-07-21 12:52:57 +0700

    anggaran controller

--- app/Http/Controllers/AnggaranController.php
+++ app/Http/Controllers/AnggaranController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AnggaranController extends Controller
 {
-    //
+    public function index(){
+        
+    }
+
+    public function insert(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }
+
 }
--- app/Http/Controllers/DakController.php
+++ app/Http/Controllers/DakController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Dak;
 use Illuminate\Http\Request;
 
 class DakController extends Controller
 {
-    //
+    
 }
