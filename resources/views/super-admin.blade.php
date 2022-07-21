15cf5b64c303a18139d306801e9166f7e061534b
faraahsm <85088236+faraahsm@users.noreply.github.com>
2022-07-21 13:49:23 +0700

    ganti nama, benakin view lagi

--- resources/views/super-admin.blade.php
+++ resources/views/super-admin.blade.php
@@ -341,7 +341,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; E-SIDAK 2022</span>
                     </div>
                 </div>
             </footer>
