a62dc4a549ff0bccb4ab0aaec115ecf861ecd827
billyalif <muhammad.nabil.alif-2020@vokasi.unair.ac.id>
2022-08-02 07:45:24 +0700

    controller tp blm selese semwa

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Batch;
 use Illuminate\Http\Request;
 
 class BatchController extends Controller
 {
-    //
+    public function index(){
+        $tabel_batch = Batch::all();
+        return view('batch-data',
+            ['tabel_bidang'=>$tabel_batch]);
+    }
+
+    public function insert(){
+        return view('batch-create',[
+            'title' => 'Tambah Batch'
+        ]);
+    }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'triwulan_batch' => 'required|max:15',
+            'tahun_batch' => 'required|max:4',
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        Batch::create($validatedData);
+
+        $request->session()->flash('success', 'Batch Baru Berhasil ditambahkan!');
+
+        return redirect('/batch-data');
+    }
+
+    public function destroy($id){
+        Batch::destroy($id);
+
+        return redirect('/batch-data')->with('successDelete', 'Batch Berhasil dihapus!');
+    }
 }
+
+
+
+
