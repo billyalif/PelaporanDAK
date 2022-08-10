f8f8a23c66bdcbb517d963390c57ab23f539c435
faraahsm <85088236+faraahsm@users.noreply.github.com>
2022-08-10 13:54:21 +0700

    nambahi seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
                 'alamat_user' => 'Jl. A. Yani No. 100, Kabupaten Banyuwangi',
                 'telp_user' => '(0333)427344',
                 'username_user' => 'adminkab.banyuwangi',
-                'password' => bcrypt('bwi12345'),
+                'password' => bcrypt('banyuwangi123'),
                 'id_role' =>'2'
             ],
 
@@ -46,7 +46,7 @@ class DatabaseSeeder extends Seeder
                 'alamat_user' => 'Jl. Sudanco Supriadi No. 17, Kabupaten Blitar, Jawa Timur',
                 'telp_user' => '(0342)802001',
                 'username_user' => 'adminkab.blitar',
-                'password' => bcrypt('adminpemkotblitar123'),
+                'password' => bcrypt('blitar123'),
                 'id_role' =>'2'
             ],
 
@@ -55,7 +55,106 @@ class DatabaseSeeder extends Seeder
                 'alamat_user' => 'Jl. P. Mas Tumapel No. 1, Kabupaten Bojonegoro, Jawa Timur',
                 'telp_user' => '(0353)881945',
                 'username_user' => 'adminkab.bojonegoro',
-                'password' => bcrypt('kotabojonegoro'),
+                'password' => bcrypt('bojonegoro123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Bondowoso',
+                'alamat_user' => 'Jl. Letjen Suprapto, Potos, Badean, Kec. Bondowoso, Kabupaten Bondowoso, Jawa Timur 68211',
+                'telp_user' => '(0353)881945',
+                'username_user' => 'adminkab.bojonegoro',
+                'password' => bcrypt('bondowoso123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Gresik',
+                'alamat_user' => 'Jl. Dr. Wahidin Sudirohusodo No. 245, Kabupaten Gresik, Jawa Timur',
+                'telp_user' => '(031)3952825',
+                'username_user' => 'adminkab.gresik',
+                'password' => bcrypt('gresik123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Jember',
+                'alamat_user' => 'Jl. Sudirman No. 1, Kabupaten Jember, Jawa Timur',
+                'telp_user' => '(0331)481945',
+                'username_user' => 'adminkab.jember',
+                'password' => bcrypt('jember123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Jombang',
+                'alamat_user' => 'Jl. K.H Wahid Hasyim No. 137A, Kaliwungu, Kec. Jombang, Kabupaten Jombang, Jawa Timur 61419',
+                'telp_user' => '(0321)861945',
+                'username_user' => 'adminkab.jombang',
+                'password' => bcrypt('jombang123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Kediri',
+                'alamat_user' => 'Jl. Soekarno-Hatta No. 1 Kediri, Jawa Timur',
+                'telp_user' => '(0354)6899015',
+                'username_user' => 'adminkab.kediri',
+                'password' => bcrypt('kediri123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Lamongan',
+                'alamat_user' => 'Jl. K.H Ahmad Dahlan No. 1 Lamongan, Jawa Timur',
+                'telp_user' => '(0322)321012',
+                'username_user' => 'adminkab.lamongan',
+                'password' => bcrypt('lamongan123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Lumajang',
+                'alamat_user' => 'Jl. Alun-Alun Utara No. 7 Lumajang, Jawa Timur',
+                'telp_user' => '(0334)881255',
+                'username_user' => 'adminkab.lumajang',
+                'password' => bcrypt('lumajang123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Madiun',
+                'alamat_user' => 'Jl. Alun-Alun No. 4, Kabupaten Madiun, Jawa Timur',
+                'telp_user' => '(0352)451325',
+                'username_user' => 'adminkab.madiun',
+                'password' => bcrypt('madiun123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Magetan',
+                'alamat_user' => 'Jl. Basuki Rahmat Timur No. 1, Kabupaten Magetan, Jawa Timur',
+                'telp_user' => '(0351)895280',
+                'username_user' => 'adminkab.magetan',
+                'password' => bcrypt('magetan123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Malang',
+                'alamat_user' => 'Jl. Merdeka Timur No. 3-5, Kota Malang, Jawa Timur',
+                'telp_user' => '(0341)326791',
+                'username_user' => 'adminkab.malang',
+                'password' => bcrypt('malang123'),
+                'id_role' =>'2'
+            ],
+
+            [
+                'nama_user' => 'Kabupaten Mojokerto',
+                'alamat_user' => 'Jl. Ahmad Yani No. 16, Mergelo, Purwotengah, Kec. Magersari, Kota Mojokerto, Jawa Timur',
+                'telp_user' => '(0321)321744',
+                'username_user' => 'adminkab.mojokerto',
+                'password' => bcrypt('mojokerto123'),
                 'id_role' =>'2'
             ]
 
